26e71dd2605cbf5f0917926cafd894a892c18426
Mike Rivet <mrivet@uship.com>
2019-06-25 12:47:12 -0500

    WIP initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import * as React from 'react';
-import { Bid } from './models/Bid';
+import { createMuiTheme, withStyles, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { generateBids } from './services/BidGenerationService';
-import { DeterminWinningBidderAndPrice } from './services/AuctionWinnerService';
 
 import './_app.css';
+
+const UShipButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '.5rem 1.875rem;',
+    border: 'none',
+    backgroundColor: '#00b437',
+    borderColor: '#00b437',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#00902c',
+      borderColor: '#00902c',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#00902c',
+      borderColor: '#00902c',
+    },
+    '&.Mui-disabled': {
+      color: '#ffffff',
+    }
+  },
+})(Button)
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 
 /** 
  * There are no properties in the main application: nobody passes it anything. 
@@ -21,74 +59,26 @@ type AppProperties = { }
  * The entry point of the application.  
  */
 export const App: React.SFC<AppProperties> = () => {
-  const [ bids, setBids ] = React.useState([] as Bid[]);
-  const [ winningBid, setWinningBid ] = React.useState({} as Bid);
-  const [ numberOfBidders, setNumberOfBidders ] = React.useState(5);
-  const [ numberOfBids, setNumberOfBids ] = React.useState(20);
-  const [ lowestBidPrice, setLowestBidPrice ] = React.useState(90);
-  const [ highestBidPrice, setHighestBidPrice ] = React.useState(200);
-  const [ auctionReservePrice, setAuctionReservePrice ] = React.useState(140);
 
-  const handleRunAuctionClick = () => {
-    const generatedBids = generateBids(numberOfBidders, numberOfBids, lowestBidPrice, highestBidPrice);
-    const winningBid = DeterminWinningBidderAndPrice(auctionReservePrice, generatedBids);
-    setWinningBid(winningBid);
-    setBids(generatedBids);
-  };
+  const classes = useStyles();
 
   return (
     <React.Fragment>
       <Container>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" color="inherit">Vickery Auction Simulator!</Typography>
-            <Link href="https://github.com/michaelrivet/vickrey-auction" color="inherit">View on Github</Link>
+            <Typography variant="h6" color="inherit">DLS In Material UI!</Typography>
           </Toolbar>
         </AppBar>
       </Container>
       <Container>
-        <TextField
-          label="Number of Bidders"
-          value={numberOfBidders}
-          onChange={e => setNumberOfBidders(+e.target.value)}
-          type="number"
-        />
-        <TextField
-          id="NumberOfBids"
-          label="Number of Bids"
-          value={numberOfBids}
-          onChange={e => setNumberOfBids(+e.target.value)}
-          type="number"
-        />
-        <TextField
-          label="Lowest Bid Price"
-          value={lowestBidPrice}
-          onChange={e => setLowestBidPrice(+e.target.value)}
-          type="number"
-        />
-        <TextField
-          label="Highest Bid Price"
-          value={highestBidPrice}
-          onChange={e => setHighestBidPrice(+e.target.value)}
-          type="number"
-        />
-        <TextField
-          label="Auction Reserve Price"
-          value={auctionReservePrice}
-          onChange={e => setAuctionReservePrice(+e.target.value)}
-          type="number"
-        />
-      </Container>
-      <Container>
-        <Button id="RunAuction" variant="contained" color="primary" onClick={handleRunAuctionClick}>Simulate Random Auction</Button>
-      </Container>
-      <Container className="bidDisplay">
-        {
-          !!winningBid.bidderId && <div className="bidDisplay-winningBid">{`Winning Bidder: ${winningBid.bidderId} - Price Paid: ${winningBid.price}`}</div>
-        }
-        {
-          bids.slice(0, 1000).map((bid, iter) => (<div className="bidDisplay-bid" key={iter}>{`${bid.bidderId} : ${bid.price}`}</div>))
-        }
+        <UShipButton variant="contained" color="primary" disableRipple className={classes.margin}>
+          DLS Button
+        </UShipButton>
+
+        <UShipButton variant="contained" color="primary" disableRipple className={classes.margin} disabled>
+          DLS Disabled
+        </UShipButton>
       </Container>
     </React.Fragment>
   )
